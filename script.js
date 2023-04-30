a9b108da99e65ffb7f7f6b3dbca37d91065a982a
Vatsal <vatrpat@gmail.com>
2023-04-30 17:57:37 -0500

    Completed basic calculator UI. No JS hooked up yet

---
+++ script.js
@@ -0,0 +1,26 @@
+
+
+
+
+
+
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(a, b, operation) {
+    return operation(a, b);
+}
